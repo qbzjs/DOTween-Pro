e3cb43a36452c9c79811a770369568a79d0b55c2
jakob Schildt <r.odgaard@gmail.com>
2021-03-17 20:44:43 +0100

    simple video player and sample video

--- Assets/VideoPlayer_script.cs
+++ Assets/VideoPlayer_script.cs
@@ -1,8 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class VideoPlayer_script : MonoBehaviour
 {
+    string URL = "Assets/Videos/The Goal - 27070.mp4";
+    VideoPlayer videoPlayer;
 
+    private void Start()
+    {
+        Init();           
+    }
+
+    private void Init()
+    {
+        GameObject camera = Camera.main.gameObject;
+        videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
+        videoPlayer.playOnAwake = true;
+        videoPlayer.url = URL;
+        videoPlayer.isLooping = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (videoPlayer.isPlaying)
+            {
+                videoPlayer.Pause();
+            }
+            else { videoPlayer.Play(); }
+        }
+    }
 }
